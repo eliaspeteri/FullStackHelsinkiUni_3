0d0438f178ff40bd01b228ec76098d112b3ce95f
Elias Peteri <elias.peteri@hotmail.com>
2021-04-06 20:50:32 +0300

    edited mongodb uri

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.x3oih.mongodb.net/person-app?retryWrites=true&w=majority`;
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, {
     useNewUrlParser: true,
